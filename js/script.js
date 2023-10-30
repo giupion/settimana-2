78a6d077c22bb70d2a6b27937020f43e35f18fda
Giuseppe Sansone <giuseppesansone144@gmail.com>
2023-10-30 10:23:40 +0100

    super

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 console.log("sono nell body della pagina raccomandata")
+
+
+var num1=25;
+var num2=15.5;
+
+var str1="questo è 'un testo"
+var str2="questo è nu testo"
+
+var somma=num1+num2;
+console.log(somma);
