187b0498f9ee517311b4d16d5326cd814662a106
Giuseppe Sansone <giuseppesansone144@gmail.com>
2023-10-30 11:05:54 +0100

    change

--- js/script.js
+++ js/script.js
@@ -4,8 +4,28 @@ console.log("sono nell body della pagina raccomandata")
 var num1=25;
 var num2=15.5;
 
-var str1="questo è 'un testo"
+var str1="questo è 'un testo'"
 var str2="questo è nu testo"
 
 var somma=num1+num2;
-console.log(somma);
+console.log(str1);
+
+var printSomma=str1+somma;
+
+
+var str3="questa è la somma";
+console.log(str3 + (num1+num2));
+
+
+var bol1=true;
+var  bol2=false;
+
+var emp1=null;
+var emp2=undefined;
+
+
+var v; //dichiarazione di una variabile scatola con nome 
+v='valore' ;// inizializzazione variabile metto valore nella scatola
+
+
+var q= "valore" //dichiarazione e inizializzazione di una variabile"
