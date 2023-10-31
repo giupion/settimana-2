c50a6e233b454ad7a381c2da411f540d220f880b
Giuseppe Sansone <giuseppesansone144@gmail.com>
2023-10-31 09:27:59 +0100

    dok

---
+++ teoria giorno 31/js/script.js
@@ -0,0 +1,13 @@
+let nome="mario"
+let cognome="rossi"
+let eta=45;
+let città="roma"
+
+let obj={
+    nome:"mario",
+    cognome:"rossi",
+    eta:45,
+    citta:"roma"
+}
+
+console.log(obj.nome)
