280e68dba30487a8c3c8e538c9dad26639a4372c
Giuseppe Sansone <giuseppesansone144@gmail.com>
2023-11-05 17:45:25 +0100

    done

--- esercizio giorno 3/js/D4-EXTRA.js
+++ esercizio giorno 3/js/D4-EXTRA.js
@@ -12,22 +12,22 @@
 
 
 
-function giveMeRandom(n) {
-    let array = [];
-    for (i = 0; i < n; i++) {
-        let casuale = Math.random() * n;
-
+function giveMeRandom(n){
+    let array=[];
+    for(i=0;i<n;i++)
+        { let casuale=Math.random()*n;
+            
         array.push(casuale);
-        function checkArray(array) {
-            for (c = 0; array.length; i++) { if (array[i] > 5) { console.log(array = array[i]) } }
-        }
-        return array;
-
+        function checkArray(array){
+            for(c=0;array.length;i++){if (array[i]>5){console.log(array=array[i])}}
+    }
+    return array;
+  
     }
 
 }
-console.log(giveMeRandom())
-
+    
+ 
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
@@ -38,40 +38,33 @@ console.log(giveMeRandom())
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
-const shoppingCart = [{
-    price: 20,
-    name: "dinosauri",
-    id: "dino",
-    quantity: 5,
-},
+const shoppingCart=[{price:20,
+name:"dinosauri",
+id:"dino",
+quantity:5,},
 
-{
-    price: 5,
-    name: "pentola",
-    id: "cucina",
-    quantity: 5,
-},
+{price:5,
+    name:"pentola",
+    id:"cucina",
+    quantity:5,},
 
-{
-    price: 11,
-    name: "draghi",
-    id: "fantasy",
-    quantity: 5,
-}];
+    {price:11,
+        name:"draghi",
+        id:"fantasy",
+        quantity:5,}];
 
-function shoppingCartTotal() {
-    let total = 0;
-    for (i = 0; i < shoppingCart.length; i++) {
-        let price = shoppingCart[i].price;
-        let quantity = shoppingCart[i].quantity;
-        total += price * quantity;
-    }
-
-    return total;
-
-}
-
-console.log(shoppingCartTotal())
+        function shoppingCartTotal(){
+let total=0;
+            for(i=0;i<shoppingCart.length;i++)
+            { let price=shoppingCart[i].price;
+            let quantity=shoppingCart[i].quantity;
+             total+=price*quantity;}
+        
+            return total;
+         
+        }
+        
+        console.log(shoppingCartTotal())
 
 /* EXTRA 3
  Nel tuo eCommerce disponi di un'array di oggetti chiamato "shoppingCart". Ognuno di questi oggetti ha le seguenti proprietà: "price", "name", "id" e "quantity".
@@ -80,23 +73,23 @@ console.log(shoppingCartTotal())
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
-function addToShoppingCart(shoppingCart) {
-
-    let newObj = {
-        price: 8,
-        name: "spadelaser",
-        id: "fantasy",
-        quantity: 5,
+function addToShoppingCart(shoppingCart){
+    
+    let newObj={
+        price:8,
+        name:"spadelaser",
+        id:"fantasy",
+        quantity:5,
     }
     shoppingCart.push(newObj);
-
+    
     return shoppingCart.length;
-}
+            }
 
-
-
-console.log(addToShoppingCart(shoppingCart))
-
+            
+            
+            console.log(addToShoppingCart(shoppingCart))
+    
 
 /* EXTRA 4
  Nel tuo eCommerce disponi di un'array di oggetti chiamato "shoppingCart". Ognuno di questi oggetti ha le seguenti proprietà: "price", "name", "id" e "quantity".
@@ -106,8 +99,15 @@ console.log(addToShoppingCart(shoppingCart))
 /* SCRIVI QUI LA TUA RISPOSTA */
 
 
+function maxShoppingCart(shoppingCart){
+ 
 
- /*EXTRA 5
+        if(shoppingCart[0].price>shoppingCart[1].price&&shoppingCart[1].price>shoppingCart[2].price&&shoppingCart[2].price>shoppingCart[3].price)
+    {return shoppingCart[0]}
+    
+}
+console.log(maxShoppingCart(shoppingCart))
+/* EXTRA 5
  Nel tuo eCommerce disponi di un'array di oggetti chiamato "shoppingCart". Ognuno di questi oggetti ha le seguenti proprietà: "price", "name", "id" e "quantity".
  Crea una funzione chiamata "latestShoppingCart" che riceve l'array "shoppingCart" e ritorna l'ultimo elemento.
 */
@@ -126,38 +126,27 @@ console.log(latestShoppingCart(shoppingCart))
 */
 
 
+//function loopUntil(x){
+   //{for(i=0;i<x;i++)
 
-
+      //randomico= Math.random()*9;
+     // if(randomico>x) 
+    
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
 /* EXTRA 7
 Crea una funzione chiamata "average" che riceve un array come parametro e ne ritorna la media aritmetica. La funzione salta automaticamente i valori non numerici nell'array.
 */
-function average(array) {
-    let somma = 0;
-    let media = 0;
-    for (i = 0; i < array.length; i++) {
-        if (typeof array[i] !== "number") { i-- } else {
-            somma += array[i];
-            media = somma / array.length;
-        }
 
 
-
-    }
-
-    return media
-
-
-}
-
-console.log(average([4, 3, 4,,"fj", 5,"f"]))
 /* SCRIVI QUI LA TUA RISPOSTA */
 
 /* EXTRA 8
  Crea una funzione chiamata "longest" che trova la stringa più lunga all'interno di un array di stringhe fornito come parametro.
 */
+
+/* SCRIVI QUI LA TUA RISPOSTA */
 
 arrayStringhe = ["aj", "fsdf", "fsddfjf", "fj", "sjffsd"]
 
@@ -174,20 +163,14 @@ function longest(arrayStringhe) {
 
     } return valoreMassimo
 }
-
-
-
 console.log(longest(arrayStringhe))
-
-
-
-/* SCRIVI QUI LA TUA RISPOSTA */
 
 /* EXTRA 9
  Crea una funzione per creare un filtro anti-spam per la tua casella email. La funzione riceve un parametro stringa chiamato "emailContent", e torna un valore booleano.
  La funzione deve ritornare true se "emailContent" non contiene le parole "SPAM" o "SCAM".
 */
 
+/* SCRIVI QUI LA TUA RISPOSTA */
 function antiSpam(emailContent) {
     if (emailContent !== "SPAM" && "SCAM") { return true }
     else if (emailContent === "SPAM" && "SCAM") { return false }
@@ -195,13 +178,9 @@ function antiSpam(emailContent) {
 
 console.log(antiSpam("GIGI"))
 
-/* SCRIVI QUI LA TUA RISPOSTA */
-
 /* EXTRA 10
  Scrivi una funzione che riceve una data come parametro, e calcola il numero di giorni passati da quella data.
 */
-
-function data(data) { }
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
